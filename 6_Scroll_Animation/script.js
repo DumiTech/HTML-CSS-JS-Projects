f79a1c89088bbff3002879c90a6c692f77fd6e47
dumi <73071227+dumiGitHub@users.noreply.github.com>
2021-09-18 12:22:43 +0300

    Add files via upload

---
+++ 6_Scroll_Animation/script.js
@@ -0,0 +1,19 @@
+const boxes = document.querySelectorAll('.box') //it will put in in an array/node list
+
+window.addEventListener('scroll', checkBoxes)
+
+checkBoxes()
+
+function checkBoxes() {
+    const triggerBottom = window.innerHeight / 5 * 4
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+
+        if(boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
+    })
+}
