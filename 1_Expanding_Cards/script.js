58fca936aabdab6732e8f2238324fd0790066db0
dumi <73071227+DumiTech@users.noreply.github.com>
2021-10-25 23:42:10 +0300

    Add files via upload

--- 1_Expanding_Cards/script.js
+++ 1_Expanding_Cards/script.js
@@ -1,17 +1,14 @@
-const panels = document.querySelectorAll('.panel')
+const panels = document.querySelectorAll('.panel');
 
-//console.log(panels[0])
-
-panels.forEach(panel => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
-})
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses();
+    panel.classList.add('active');
+  });
+});
 
 function removeActiveClasses() {
-    panels.forEach(panel => {
-        panel.classList.remove('active')
-    })
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+  });
 }
-
